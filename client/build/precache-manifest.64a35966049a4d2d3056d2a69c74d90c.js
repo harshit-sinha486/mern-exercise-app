f9437f822f05fbbc02fac5daab10d4a3018f4655
Harshit Sinha <hsinha2547.ca18@chitkara.edu.in>
2020-08-18 03:42:48 +0530

    Retry

---
+++ client/build/precache-manifest.64a35966049a4d2d3056d2a69c74d90c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "820b4cc706780aa6f28685024f738e40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2af1d43d708184527341",
+    "url": "/static/css/2.bf5b17f7.chunk.css"
+  },
+  {
+    "revision": "2af1d43d708184527341",
+    "url": "/static/js/2.4743e14a.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.4743e14a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "188ad0a495d4fa5a8ef8",
+    "url": "/static/js/main.66fa1624.chunk.js"
+  },
+  {
+    "revision": "96528b5f4ae212fa84dc",
+    "url": "/static/js/runtime-main.526c36d9.js"
+  }
+]);
