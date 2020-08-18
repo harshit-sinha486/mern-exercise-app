17f3f24eb305412412ee36eef7e6414a645c8912
Harshit Sinha <hsinha2547.ca18@chitkara.edu.in>
2020-08-19 03:18:54 +0530

    app.set

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path')
-
+const https = require('https');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT||5000;
+app.set(PORT, () => console.log(`Server is listening on port ${PORT}...`));
+https.createServer(app).listen(80);
+https.createServer(app).listen(443);
 
 app.use(cors());
 app.use(express.json());
@@ -36,6 +39,6 @@ if(process.env.NODE_ENV === 'production'){
   
 }
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server is running on port: ${port}`);
+// });
