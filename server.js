4f53b7cb1f39ec234b0af45ae3d6d8331bd0c31d
Harshit Sinha <hsinha2547.ca18@chitkara.edu.in>
2020-08-18 08:36:35 +0530

    path set to index

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path')
 
 require('dotenv').config();
 
@@ -29,6 +30,10 @@ app.use('/users', usersRouter);
 
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
+
+  app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
   
 }
 
