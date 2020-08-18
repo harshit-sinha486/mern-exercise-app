4cb9561b3dbcdd7ff917ebaaf2fb5bdbeef125a2
Harshit Sinha <hsinha2547.ca18@chitkara.edu.in>
2020-08-18 21:23:54 +0530

    server.js changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGODB_URI;
+const uri = 'mongodb+srv://harshit:2bpJhDewafbfhbJ@v1-olhzv.mongodb.net/exersise-app?retryWrites=true&w=majority';
 try {
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
