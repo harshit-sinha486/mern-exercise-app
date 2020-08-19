83916d5d9892f000875ec0ca4b55e5259d58d03e
Harshit Sinha <hsinha2547.ca18@chitkara.edu.in>
2020-08-19 22:41:59 +0530

    BUILD deleted

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ if(process.env.NODE_ENV === 'production'){
   
 }
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port: ${process.env.PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
 });
