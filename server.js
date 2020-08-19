4340604705ec3004611a82956b60a3dfcacbc774
Harshit Sinha <hsinha2547.ca18@chitkara.edu.in>
2020-08-20 03:33:03 +0530

    add static server

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req,res)=>{
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
