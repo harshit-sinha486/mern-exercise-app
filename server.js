c1648d063d50a8be4c1640b4f5d155f12938aa66
Harshit Sinha <hsinha2547.ca18@chitkara.edu.in>
2020-08-19 08:10:39 +0530

    Port fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path')
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -37,6 +37,6 @@ if(process.env.NODE_ENV === 'production'){
   
 }
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port: ${process.env.PORT}`);
 });
