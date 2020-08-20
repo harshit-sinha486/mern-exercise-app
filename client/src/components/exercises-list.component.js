a9bab7648a8a3180631caeb0357f63fd0e5e70d5
Harshit Sinha <hsinha2547.ca18@chitkara.edu.in>
2020-08-20 10:58:54 +0530

    dotenv config

--- client/src/components/exercises-list.component.js
+++ client/src/components/exercises-list.component.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 require('dotenv').config();
-
+var port = parseInt(process.env.PORT);
 const Exercise = props => (
     <tr>
       <td>{props.exercise.username}</td>
