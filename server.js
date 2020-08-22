706343555a4afbc7cefcadce9221e94bc31b888b
Harshit Sinha <hsinha2547.ca18@chitkara.edu.in>
2020-08-22 10:11:45 +0530

    back to basics

--- server.js
+++ server.js
@@ -28,14 +28,14 @@ const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 
 
-// if(process.env.NODE_ENV == 'production'){
-//   app.use(express.static('client/build'));
+if(process.env.NODE_ENV == 'production'){
+  app.use(express.static('client/build'));
 
-//   app.get('*', (req,res)=>{
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   })
+  app.get('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
   
-// }
+}
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
